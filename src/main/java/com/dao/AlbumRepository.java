82a784390e25f3810a7d2e987db7bfcd5443cbd4
melkir <melkir13@gmail.com>
2015-03-30 22:02:23 +0200

    fix bugs

---
+++ src/main/java/com/dao/AlbumRepository.java
@@ -0,0 +1,13 @@
+package com.dao;
+
+import com.bean.Album;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+/**
+ * Created by melkir on 30/03/15.
+ */
+public interface AlbumRepository extends CrudRepository<Album, Integer> {
+    List<Album> findAll();
+}
